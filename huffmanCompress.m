acf32601d7d26885cfdce44e75732a202f6481a0
Archit Puri <architpuri.7@gmail.com>
2019-04-24 12:03:28 +0530

    Update huffmanCompress.m

--- huffmanCompress.m
+++ huffmanCompress.m
@@ -1,26 +1,38 @@
+
 iStart = imread('lena.jpg');
-iBlock=zeros(4,4);
-for i=1:4
-    for j=1:4
-        iBlock(i,j) = iStart(i,j);
-    end
-end
-symbols=161:163;
+iBlock=iStart(1:4,1:4);
+symbolsA= transpose(unique(iStart(1:4,1:4)));
+symbols=double(symbolsA);
 p=[0.1875 0.5 0.3125];
 dict = huffmandict(symbols,p);
 sig = randsrc(5,1,[symbols; p]);
 comp = huffmanenco(sig,dict);
 iOutput=size(iBlock);
-%dsig = huffmandeco(comp,dict);
-%isequal(sig,dsig)
 valueInc=0;
 for i=1:4
     for j=1:4
         if(valueInc < length(comp))
             valueInc=valueInc+1;
         iOutput(i,j)=comp(valueInc);
-        else
+        elseif(i==4 && j==4)
+            iOutput(i,j)=length(comp);
+        else 
             iOutput(i,j)=5;
         end
     end
 end
+
+lengthImage=iOutput(4,4);
+compObtained=zeros(lengthImage,1);
+valueObtainedInc=1;
+for i=1:4
+    for j=1:4
+        if(valueObtainedInc < lengthImage+1)
+            compObtained(valueObtainedInc,1)=iOutput(i,j);
+            valueObtainedInc=valueObtainedInc+1;
+        end
+    end
+end
+dsig = huffmandeco(compObtained,dict);
+isequal(sig,dsig)
+symbolsf= double(transpose(unique(dsig)));
